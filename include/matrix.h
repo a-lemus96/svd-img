3b5d6ae1389839aff680630e4461196aa226f816
a-lemus96 <alejandro.lemus@cimat.mx>
2024-03-01 21:23:49 -0600

    [feat]: Create Matrix interface
    Define class interface in .h file. Implementation is still pending, this
    commit is related to #4.

---
+++ include/matrix.h
@@ -0,0 +1,31 @@
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#include  <vector>
+
+template <typename T>
+class Matrix {
+    private:
+        std::vector<std::vector<T>> data;
+        int rows;
+        int cols;
+
+    public:
+        Matrix(int nRows, int nCols);               // constructor
+        Matrix(const Matrix<T>& other);             // copy constructor
+        ~Matrix();                                  // destructor
+
+        std::vector<T>& operator[](int row);                    // subscript
+        const std::vector<T>& operator[](int row) const;
+        Matrix<T> operator+(const Matrix<T>& other) const;      // sum
+        Matrix<T> operator-(const Matrix<T>& other) const;      // substract
+        Matrix<T> operator*(const Matrix<T>& other) const;      // product
+
+        Matrix<T> transpose() const;    // compute transpose
+
+        std::vector<int> shape() const; // get matrix shape (nRows, nCols)
+        int nRows() const;              // get number of rows
+        int nCols() const;              // get number of cols
+
+        void print() const;     // print the matrix
+}
