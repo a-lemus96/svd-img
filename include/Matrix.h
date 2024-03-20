58d24d6c2f4c972707d91b4fe9353fef666a745f
a-lemus96 <alejandro.lemus@cimat.mx>
2024-03-20 13:33:32 -0600

    [feat]: Implement template in .h file
    - Remove Matrix.cpp file and include everything in .h file
    - Implement operators as nonmember functions
    - Add product by scalar operation

--- include/Matrix.h
+++ include/Matrix.h
@@ -6,30 +6,124 @@
 
 template <typename T>
 class Matrix {
+    Matrix<T> operator+(const Matrix<T>& lhs, const Matrix<T>& rhs) const {
+        // sanity check
+        if (lhs.rows != rhs.rows || lhs.cols != rhs.cols) {
+            throw std::invalid_argument(
+                    "Matrices must have the same dimensions.");
+        }
+        Matrix<T> result(lhs.rows, lhs.cols);
+        for (int i = 0; i < lhs.rows; ++i) {
+            for (int j = 0; j < lhs.cols; ++j) {
+                result[i][j] = lhs[i][j] + rhs[i][j];
+            }
+        }
+        return result;
+    }
+
+    Matrix<T> operator-(const Matrix<T>& lhs, const Matrix<T>& rhs) const {
+        return lhs + (-1 * rhs);
+
+    Matrix<T> operator*(const T& k, const Matrix<T> rhs) const {
+        Matrix<T> result(rhs.rows, rhs.cols);
+        for (int i = 0; i < rhs.rows; ++i) {
+            for (int j = 0; j < rhs.rows; ++j) {
+                result[i][j] = k * rhs[i][j];
+            }
+        }
+        return result;
+    }
+
+    Matrix<T> operator*(const Matrix<T> lhs, const T& k) const {
+        return k * lhs;
+
+    Matrix<T> operator*(const Matrix<T>& lhs, const Matrix<T>& rhs) const {
+        // sanity check
+        if (lhs.cols != rhs.rows) {
+            throw std::invalid_argument(
+                    "Number of left cols must equal number of right rows.");
+        }
+        Matrix<T> result(lhs.rows, rhs.cols);
+        T temp;   // stores partial sums
+        for (int i = 0; i < lhs.rows; ++i) {
+            for (int j = 0; j < rhs.cols; ++j) {
+                temp = 0;
+                for (int k = 0; k < lhs.cols; ++k) {
+                    temp += data[i][k] * other[k][j];
+                }
+                result[i][j] = temp;
+            }
+        }
+        return result;
+    }
+
     private:
         std::vector<std::vector<T>> data;
         int rows;
         int cols;
 
     public:
-        Matrix(int nRows, int nCols);                           // constructors
-        Matrix(const std::vector<std::vector<T>>& initData);
-        Matrix(const Matrix<T>& other);
-        ~Matrix();                                              // destructor
+        // constructors
+        Matrix() {}
 
-        std::vector<T>& operator[](int row);                    // subscript
-        const std::vector<T>& operator[](int row) const;
-        Matrix<T> operator+(const Matrix<T>& other) const;      // sum
-        Matrix<T> operator-(const Matrix<T>& other) const;      // substract
-        Matrix<T> operator*(const Matrix<T>& other) const;      // product
+        Matrix(int nRows, int nCols) 
+            : rows(nRows),
+              cols(nCols) {
+            data.resize(rows, std::vector<T>(cols));
+        }
 
-        Matrix<T> transpose() const;    // compute transpose
+        Matrix(const std::vector<std::vector<T>>& initData)
+            : rows(initData.size()),
+              cols(initData[0].size()),
+              data(initData) {}
 
-        std::vector<int> shape() const; // get matrix shape (nRows, nCols)
-        int nRows() const;              // get number of rows
-        int nCols() const;              // get number of cols
+        Matrix(const Matrix<T>& other) 
+            : rows(other.rows),
+              cols(other.cols),
+              data(other.data) {}
 
-        void print() const;     // print the matrix
+        // destructor
+        ~Matrix() {}
+
+        // operators
+        std::vector<T>& operator[](int row) {
+            return data[row];
+        }
+
+        const std::vector<T>& operator[](int row) const {
+            return data[row];
+        }
+
+        Matrix<T> transpose() const {
+            Matrix<T> result(cols, rows);
+            for (int i = 0; i < rows; ++i) {
+                for (int j = 0; j < cols; ++j) {
+                    result[j][i] = data[i][j];
+                }
+            }
+            return result;
+        }
+
+        std::vector<int> shape() const {
+            return std::vector<int>{rows, cols};
+        }
+
+        int nRows() const {
+            return rows;
+        }
+
+        int nCols() const {
+            return cols;
+        }
+
+        void print() const {
+            for (int i = 0; i < rows; ++i) {
+                for (int j = 0; j < cols; ++j) {
+                    std::cout << data[i][j] << ' ';
+                }
+                std::cout << std::endl;
+            }
+        }
 };
 
 #endif
